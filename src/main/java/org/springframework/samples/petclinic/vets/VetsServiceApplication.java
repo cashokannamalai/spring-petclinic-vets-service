06679ac326fa9986307a96c5d23034e51fba1dee
cashokannamalai <166033475+cashokannamalai@users.noreply.github.com>
2024-06-25 13:37:42 +0530

    Update VetsServiceApplication.java

--- src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -19,8 +19,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.samples.petclinic.vets.model.VetRepository;
 import org.springframework.samples.petclinic.vets.system.VetsProperties;
 
 /**
